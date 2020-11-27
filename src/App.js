a346e27edb788ac7d8a988ce24965b58515bd507
Luiz Daniel Lima <luizdml@gmail.com>
2020-11-27 10:12:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
+import { Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  titleStyle: {
+    fontStyle: 'oblique',
+    marginBottom: '2rem'
+  }
+})
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Typography 
+            variant="h4"
+            color="primary"
+            className={classes.titleStyle}
+          >
+            This is the title
+          </Typography>
+          <Button color="primary" variant="outlined">click me!</Button>
+        </header>
+      </div>
+    </ThemeProvider>
+
   );
 }
 
